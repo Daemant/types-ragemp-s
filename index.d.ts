58629593cde9c99e48c65845a529f6b0eb89539d
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2020-02-10 11:56:25 +0200

    Исправление ошибок типизации

--- index.d.ts
+++ index.d.ts
@@ -8,11 +8,11 @@
 // -------------------------------------------------------------------------
 
 type HashOrString = number | string;
-type RGB = [ number, number, number ];
-type RGBA = [ number, number, number, number ];
-type Array2d = [ number, number ];
-type Array3d = [ number, number, number ];
-type Array4d = [ number, number, number, number ];
+type RGB = [number, number, number];
+type RGBA = [number, number, number, number];
+type Array2d = [number, number];
+type Array3d = [number, number, number];
+type Array4d = [number, number, number, number];
 
 // -------------------------------------------------------------------------
 // Main MP type
@@ -29,17 +29,20 @@ type Mp = {
 	players: PlayerMpPool;
 	objects: ObjectMpPool;
 	vehicles: VehicleMpPool;
-	config: ConfigMp,
+	config: ConfigMp;
 	world: WorldMp;
 
-	Event: { 
-		new(eventName: RageEnums.EventKey | string, callback: (...args: any[]) => void): EventMp 
+	Event: {
+		new (
+			eventName: RageEnums.EventKey | string,
+			callback: (...args: any[]) => void
+		): EventMp;
 	};
 	Vector3: Vector3Mp;
 
 	joaat(str: string): number;
 	joaat(strs: string[]): number[];
-}
+};
 
 // -------------------------------------------------------------------------
 // Entity MP types
@@ -86,7 +89,7 @@ interface EntityMp {
 	position: Vector3Mp;
 	readonly id: number;
 	readonly type: RageEnums.EntityType;
-	
+
 	getVariable(name: string): any | undefined;
 	destroy(): void;
 	dist(position: Vector3Mp): number;
@@ -146,32 +149,42 @@ interface PlayerMp extends EntityMp {
 	readonly voiceListeners: PlayerMp[];
 
 	ban(reason: string): void;
-	call(eventName: string, ...args: any[]): void;
+	call(eventName: string, args: any[]): void;
 	clearDecorations(): void;
 	disableVoiceTo(targetPlayer: PlayerMp): void;
 	enableVoiceTo(targetPlayer: PlayerMp): void;
-	getClothes(component: RageEnums.ClothesComponent | number): {
-		drawable: number,
-		texture: number,
-		palette: number
+	getClothes(
+		component: RageEnums.ClothesComponent | number
+	): {
+		drawable: number;
+		texture: number;
+		palette: number;
 	};
 	getDecoration(collection: number): number;
 	getFaceFeature(index: number): number;
 	getHeadBlend(): {
-		shapes: number[],
-		skins: number[],
-		shapeMix: number,
-		skinMix: number,
-		thirdMix: number
+		shapes: number[];
+		skins: number[];
+		shapeMix: number;
+		skinMix: number;
+		thirdMix: number;
 	};
 	getHeadOverlay(overlay: RageEnums.HeadOverlay | number): Array4d;
-	getProp(prop: RageEnums.PlayerProp | number): {
-		drawable: number,
-		texture: number
+	getProp(
+		prop: RageEnums.PlayerProp | number
+	): {
+		drawable: number;
+		texture: number;
 	};
 	getWeaponAmmo(weapon: RageEnums.Hashes.Weapon | HashOrString): number;
-	giveWeapon(weaponHash: RageEnums.Hashes.Weapon | HashOrString, ammo: number): void;
-	giveWeapon(weaponHashes: (RageEnums.Hashes.Weapon | HashOrString)[], ammo: number): void;
+	giveWeapon(
+		weaponHash: RageEnums.Hashes.Weapon | HashOrString,
+		ammo: number
+	): void;
+	giveWeapon(
+		weaponHashes: (RageEnums.Hashes.Weapon | HashOrString)[],
+		ammo: number
+	): void;
 	isStreamed(player: PlayerMp): boolean;
 	invoke(hash: string, ...args: any[]): void;
 	kick(reason: string): void;
@@ -179,25 +192,61 @@ interface PlayerMp extends EntityMp {
 	outputChatBox(message: string): void;
 	playAnimation(dict: string, name: string, speed: number, flag: number): void;
 	stopAnimation(): void;
-	putIntoVehicle(vehicle: VehicleMp, seat: RageEnums.VehicleSeat | number): void;
+	putIntoVehicle(
+		vehicle: VehicleMp,
+		seat: RageEnums.VehicleSeat | number
+	): void;
 	removeAllWeapons(): void;
 	removeDecoration(decoration: number, collection: number): void;
 	removeFromVehicle(): void;
 	removeObject(object: any): void; // TODO
 	removeWeapon(weaponHash: RageEnums.Hashes.Weapon | HashOrString): void;
-	setClothes(component: RageEnums.ClothesComponent | number, drawable: number, texture: number, palette: number): void;
-	setCustomization(gender: boolean, shapeFirst: number, shapeSecond: number, shapeThird: number, skinFirst: number,
-		skinSecond: number, skinThird: number, shapeMix: number, skinMix: number, thirdMix: number, eyeColor: number,
-		hairColor: number, hightlightColor: number, faceFeatures: number[]
+	setClothes(
+		component: RageEnums.ClothesComponent | number,
+		drawable: number,
+		texture: number,
+		palette: number
+	): void;
+	setCustomization(
+		gender: boolean,
+		shapeFirst: number,
+		shapeSecond: number,
+		shapeThird: number,
+		skinFirst: number,
+		skinSecond: number,
+		skinThird: number,
+		shapeMix: number,
+		skinMix: number,
+		thirdMix: number,
+		eyeColor: number,
+		hairColor: number,
+		hightlightColor: number,
+		faceFeatures: number[]
 	): void;
 	setDecoration(collection: number, overlay: number): void;
 	setFaceFeature(index: number, scale: number): void;
 	setHairColor(firstColor: number, secondColor: number): void;
-	setHeadBlend(shapeFirst: number, shapeSecond: number, shapeThird: number, skinFirst: number, skinSecond: number,
-		skinThird: number, shapeMix: number, skinMix: number, thirdMix: number): void;
+	setHeadBlend(
+		shapeFirst: number,
+		shapeSecond: number,
+		shapeThird: number,
+		skinFirst: number,
+		skinSecond: number,
+		skinThird: number,
+		shapeMix: number,
+		skinMix: number,
+		thirdMix: number
+	): void;
 	setHeadOverlay(overlay: RageEnums.HeadOverlay | number, value: Array4d): void;
-	setProp(prop: RageEnums.PlayerProp | number, drawable: number, texture: number): void;
-	setWeaponAmmo(weapon: RageEnums.Hashes.Weapon | HashOrString, ammo: number): void;
+	setProp(
+		prop: RageEnums.PlayerProp | number,
+		drawable: number,
+		texture: number
+	): void;
+	setWeaponAmmo(
+		weapon: RageEnums.Hashes.Weapon | HashOrString,
+		ammo: number
+	): void;
 	spawn(position: Vector3Mp): void;
 	updateHeadBlend(shapeMix: number, skinMix: number, thirdMix: number): void;
 	playScenario(scenario: string): void;
@@ -239,7 +288,7 @@ interface VehicleMp extends EntityMp {
 	readonly extras: boolean[];
 	readonly heading: number;
 	readonly mods: number[];
-	readonly quaternion: QuaternionMp,
+	readonly quaternion: QuaternionMp;
 	readonly streamedPlayers: PlayerMp[];
 	readonly trailer: VehicleMp;
 	readonly traileredBy: VehicleMp;
@@ -257,11 +306,23 @@ interface VehicleMp extends EntityMp {
 	playScenario(scenario: string): void;
 	repair(): void;
 	setColor(primary: number, secondary: number): void;
-	setColorRGB(red1: number, green1: number, blue1: number, red2: number, green2: number, blue2: number): void;
+	setColorRGB(
+		red1: number,
+		green1: number,
+		blue1: number,
+		red2: number,
+		green2: number,
+		blue2: number
+	): void;
 	setExtra(index: number, value: boolean): void;
 	setMod(modType: number, modIndex: number): void;
 	setNeonColor(red: number, green: number, blue: number): void;
-	setPaint(primaryType: number, primaryColor: number, secondaryType: number, secondaryColor: number): void;
+	setPaint(
+		primaryType: number,
+		primaryColor: number,
+		secondaryType: number,
+		secondaryColor: number
+	): void;
 	setOccupant(seat: number, player: PlayerMp): void;
 	spawn(position: Vector3Mp, heading: number): void;
 }
@@ -272,21 +333,24 @@ interface VehicleMp extends EntityMp {
 
 interface WorldMp {
 	weather: RageEnums.Weather | string;
-	time: { 
-		hour: number,
-		minute: number,
-		second: number
-		
+	time: {
+		hour: number;
+		minute: number;
+		second: number;
+
 		set(hour: number, minute: number, second: number): void;
 	};
 	trafficLights: {
-		locked: boolean,
+		locked: boolean;
 		state: number;
 	};
 
 	removeIpl(name: string): void;
 	requestIpl(name: string): void;
-	setWeatherTransition(weather: RageEnums.Weather | string, duration?: number): void;
+	setWeatherTransition(
+		weather: RageEnums.Weather | string,
+		duration?: number
+	): void;
 }
 
 interface EventMp {
@@ -294,31 +358,31 @@ interface EventMp {
 }
 
 interface ConfigMp {
-	announce: boolean,
-	bind: string,
-	gamemode: string,
-	encryption: boolean,
-	maxplayers: number,
-	name: string,
-	'stream-distance': number,
-	port: number,
-	'disallow-multiple-connections-per-ip': boolean,
-	'limit-time-of-connections-per-ip': number,
-	url: string,
-	language: string,
-	'sync-rate': number,
-	'resource-scan-thread-limit': number,
-	'max-ping': number,
-	'min-fps': number,
-	'max-packet-loss': number,
-	'allow-cef-debugging': boolean,
-	'enable-nodejs': boolean,
-	'csharp': boolean,
-	'enable-http-security': boolean,
-	'voice-chat': boolean,
-	'allow-voice-chat-input': number,
-	'voice-chat-sample-rate': number,
-	'fastdl-host': string,
+	announce: boolean;
+	bind: string;
+	gamemode: string;
+	encryption: boolean;
+	maxplayers: number;
+	name: string;
+	'stream-distance': number;
+	port: number;
+	'disallow-multiple-connections-per-ip': boolean;
+	'limit-time-of-connections-per-ip': number;
+	url: string;
+	language: string;
+	'sync-rate': number;
+	'resource-scan-thread-limit': number;
+	'max-ping': number;
+	'min-fps': number;
+	'max-packet-loss': number;
+	'allow-cef-debugging': boolean;
+	'enable-nodejs': boolean;
+	csharp: boolean;
+	'enable-http-security': boolean;
+	'voice-chat': boolean;
+	'allow-voice-chat-input': number;
+	'voice-chat-sample-rate': number;
+	'fastdl-host': string;
 }
 
 // -------------------------------------------------------------------------
@@ -326,33 +390,55 @@ interface ConfigMp {
 // -------------------------------------------------------------------------
 
 interface BlipMpPool extends EntityMpPool<BlipMp> {
-	"new"(sprite: number, position: Vector3Mp, options?: {
-		alpha?: number,
-		color?: number,
-		dimension?: number,
-		drawDistance?: number,
-		name?: string,
-		rotation?: number,
-		scale?: number,
-		shortRange?: boolean
-	}): BlipMp;
+	'new'(
+		sprite: number,
+		position: Vector3Mp,
+		options?: {
+			alpha?: number;
+			color?: number;
+			dimension?: number;
+			drawDistance?: number;
+			name?: string;
+			rotation?: number;
+			scale?: number;
+			shortRange?: boolean;
+		}
+	): BlipMp;
 }
 
 interface CheckpointMpPool extends EntityMpPool<CheckpointMp> {
-	"new"(type: number, position: Vector3Mp, radius: number, options?: {
-		color?: RGBA,
-		dimension?: number,
-		direction?: Vector3Mp,
-		visible?	: boolean
-	}): CheckpointMp;
+	'new'(
+		type: number,
+		position: Vector3Mp,
+		radius: number,
+		options?: {
+			color?: RGBA;
+			dimension?: number;
+			direction?: Vector3Mp;
+			visible?: boolean;
+		}
+	): CheckpointMp;
 }
 
 interface ColshapeMpPool extends EntityMpPool<ColshapeMp> {
 	newCircle(x: number, y: number, range: number): ColshapeMp;
-	newCuboid(x: number, y: number, z: number, width: number, depth: number, height: number): ColshapeMp;
+	newCuboid(
+		x: number,
+		y: number,
+		z: number,
+		width: number,
+		depth: number,
+		height: number
+	): ColshapeMp;
 	newRectangle(x: number, y: number, width: number, height: number): ColshapeMp;
 	newSphere(x: number, y: number, z: number, range: number): ColshapeMp;
-	newTube(x: number, y: number, z: number, range: number, height: number): ColshapeMp;
+	newTube(
+		x: number,
+		y: number,
+		z: number,
+		range: number,
+		height: number
+	): ColshapeMp;
 }
 
 interface EntityMpPool<TEntity> {
@@ -363,17 +449,38 @@ interface EntityMpPool<TEntity> {
 	at(index: number): TEntity;
 	exists(entity: TEntity | number): boolean;
 	forEach(fn: (entity: TEntity) => void): void;
-	forEachInRange(position: Vector3Mp, range: number, fn: (entity: TEntity) => void): void;
-	forEachInRange(position: Vector3Mp, range: number, dimension: number, fn: (entity: TEntity) => void): void;
+	forEachInRange(
+		position: Vector3Mp,
+		range: number,
+		fn: (entity: TEntity) => void
+	): void;
+	forEachInRange(
+		position: Vector3Mp,
+		range: number,
+		dimension: number,
+		fn: (entity: TEntity) => void
+	): void;
 	forEachInDimension(dimension: number, fn: (entity: TEntity) => void): void;
 	toArray(): TEntity[];
 }
 
 interface EventMpPool {
-	add(eventName: RageEnums.EventKey | string, callback: (...args: any[]) => void): void;
-	add(events: ({ [name: string]: (...args: any[]) => void; })): void;
-	addCommand(commandName: string, callback: (player: PlayerMp, fullText: string, ...args: string[]) => void): void;
-	addCommand(commands: { [commandName: string]: (player: PlayerMp, fullText: string, ...args: string[]) => void; }): void;
+	add(
+		eventName: RageEnums.EventKey | string,
+		callback: (...args: any[]) => void
+	): void;
+	add(events: { [name: string]: (...args: any[]) => void }): void;
+	addCommand(
+		commandName: string,
+		callback: (player: PlayerMp, fullText: string, ...args: string[]) => void
+	): void;
+	addCommand(commands: {
+		[commandName: string]: (
+			player: PlayerMp,
+			fullText: string,
+			...args: string[]
+		) => void;
+	}): void;
 	call(eventName: string, ...args: any[]): void;
 	getAllOf(eventName: string): EventMp[];
 	remove(eventName: string, handler?: (...args: any[]) => void): void;
@@ -382,31 +489,45 @@ interface EventMpPool {
 }
 
 interface MarkerMpPool extends EntityMpPool<MarkerMp> {
-	"new"(type: number, position: Vector3Mp, scale: number, options?: {
-		color?: RGBA,
-		dimension?: number,
-		direction?: Vector3Mp,
-		rotation?: Vector3Mp,
-		visible?: boolean
-	}): MarkerMp;
+	'new'(
+		type: number,
+		position: Vector3Mp,
+		scale: number,
+		options?: {
+			color?: RGBA;
+			dimension?: number;
+			direction?: Vector3Mp;
+			rotation?: Vector3Mp;
+			visible?: boolean;
+		}
+	): MarkerMp;
 }
 
 interface ObjectMpPool extends EntityMpPool<ObjectMp> {
-	"new"(model: HashOrString, position: Vector3Mp, options?: {
-		alpha?: number,
-		dimension?: number,
-		rotation?: Vector3Mp
-	}): ObjectMp;
+	'new'(
+		model: HashOrString,
+		position: Vector3Mp,
+		options?: {
+			alpha?: number;
+			dimension?: number;
+			rotation?: Vector3Mp;
+		}
+	): ObjectMp;
 }
 
 interface PickupMpPool extends EntityMpPool<PickupMp> {
-	"new"(...args: any[]): PickupMp; // TODO
+	'new'(...args: any[]): PickupMp; // TODO
 }
 
 interface PlayerMpPool extends EntityMpPool<PlayerMp> {
 	broadcast(text: string): void;
 	broadcastInRange(position: Vector3Mp, range: number, text: string): void;
-	broadcastInRange(position: Vector3Mp, range: number, dimension: number, text: string): void;
+	broadcastInRange(
+		position: Vector3Mp,
+		range: number,
+		dimension: number,
+		text: string
+	): void;
 	call(eventName: string, ...args: any[]): void;
 	call(players: PlayerMp[], eventName: string, ...args: any[]): void;
 	callInDimension(eventName: string, ...args: any[]): void;
@@ -414,25 +535,33 @@ interface PlayerMpPool extends EntityMpPool<PlayerMp> {
 }
 
 interface TextLabelMpPool extends EntityMpPool<TextLabelMp> {
-	"new"(text: string, position: Vector3Mp, options?: {
-		color?: RGBA,
-		dimension?: number,
-		drawDistance?: number,
-		font?: number,
-		los?: boolean
-	}): TextLabelMp;
+	'new'(
+		text: string,
+		position: Vector3Mp,
+		options?: {
+			color?: RGBA;
+			dimension?: number;
+			drawDistance?: number;
+			font?: number;
+			los?: boolean;
+		}
+	): TextLabelMp;
 }
 
 interface VehicleMpPool extends EntityMpPool<VehicleMp> {
-	"new"(model: RageEnums.Hashes.Vehicle | HashOrString, position: Vector3Mp, options?: {
-		alpha?: number,
-		color?: [ Array2d, Array2d ] | [ RGB, RGB ],
-		dimension?: number,
-		engine?: boolean,
-		heading?: number;
-		locked?: boolean,
-		numberPlate?: string
-	}): VehicleMp;
+	'new'(
+		model: RageEnums.Hashes.Vehicle | HashOrString,
+		position: Vector3Mp,
+		options?: {
+			alpha?: number;
+			color?: [Array2d, Array2d] | [RGB, RGB];
+			dimension?: number;
+			engine?: boolean;
+			heading?: number;
+			locked?: boolean;
+			numberPlate?: string;
+		}
+	): VehicleMp;
 }
 
 // -------------------------------------------------------------------------
@@ -440,7 +569,7 @@ interface VehicleMpPool extends EntityMpPool<VehicleMp> {
 // -------------------------------------------------------------------------
 
 type Vector3Mp = {
-	new(x: number, y: number, z: number): Vector3Mp;
+	new (x: number, y: number, z: number): Vector3Mp;
 
 	x: number;
 	y: number;
@@ -456,20 +585,20 @@ type Vector3Mp = {
 	subtract(value: number): Vector3Mp;
 	subtract(vector3: Vector3Mp): Vector3Mp;
 	unit(): Vector3Mp;
-}
+};
 
 type PlayerWeaponCollectionMp = {
 	current: number;
 
 	reset(): void;
-}
+};
 
 type QuaternionMp = {
 	x: number;
 	y: number;
 	z: number;
 	w: number;
-}
+};
 
 // -------------------------------------------------------------------------
 // Default typings extends
